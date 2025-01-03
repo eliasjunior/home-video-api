b4acdda635d6e4141e5508dfd2763ba5e099c045
eliasjunior <emcjunior@gmail.com>
2025-01-03 21:35:15 +0100

    add ip convert

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv");
 
-
 import { logD } from "./common/MessageUtil";
 import { USER_LOCATION } from "./common/AppServerConstant";
 
@@ -16,14 +15,12 @@ if (process.env.NODE_ENV === "development") {
 
 export default function config() {
   const {
-    SERVER_HOST,
     SERVER_PORT,
     IMG_FOLDER_FALL_BACK,
     VIDEO_PATH,
     SERVER_PROTOCOL,
     MOVIES_DIR,
     SERIES_DIR,
-    IMAGES_HOST_SERVER,
     IMAGES_PORT_SERVER,
     IMAGE_MAP,
   } = process.env;
@@ -34,7 +31,7 @@ export default function config() {
 
   result.protocol = SERVER_PROTOCOL;
   result.port = SERVER_PORT;
-  result.host = SERVER_HOST;
+  result.host = getLocalIPAddress();
   // in case you want read the images/posters from the another folder.
   result.imgFolderFallBack = IMG_FOLDER_FALL_BACK;
   result.videosPath = VIDEO_PATH;
@@ -43,12 +40,29 @@ export default function config() {
   result.baseLocation = USER_LOCATION;
   result.serverUrl = `${result.protocol}://${result.host}:${result.port}`;
   //NGINX and it's not the images url in the AppServerConstant that has /images
-  result.imageServerHost = IMAGES_HOST_SERVER;
+  result.imageServerHost = result.host;
   result.imagePort = IMAGES_PORT_SERVER;
   result.imageMapFileName = IMAGE_MAP;
 
   logD("config result", result);
   return result;
 }
+
+function getLocalIPAddress() {
+  const os = require("os");
+
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    for (const iface of networkInterfaces[interfaceName]) {
+      // Look for the IPv4, non-internal address
+      if (iface.family === "IPv4" && !iface.internal) {
+        console.log("Local Host IP:", iface.address);
+        return iface.address;
+      }
+    }
+  }
+  return "127.0.0.1"; // Fallback to localhost
+}
+
 
 
